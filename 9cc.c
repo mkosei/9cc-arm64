28e7c764695ac9295bebecc653fa3499e246b922
Kosei Masuda <kouseikiyo0725@gmail.com>
2026-02-10 23:39:41 +0900

    パーサ実装

--- 9cc.c
+++ 9cc.c
@@ -138,6 +138,39 @@ Node *new_node_num(int val) {
   return node;
 }
 
+Node *expr() {
+  Node *node = mul();
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
 
 int main(int argc, char **argv) {
   if (argc != 2) {
